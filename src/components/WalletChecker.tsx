a3ba5d638697da3a18b88584f62d29e2b3e14619
CACTUSDIG <161220090+CACTUSDIG@users.noreply.github.com>
2024-12-23 22:40:33 +0000

    styling

--- src/components/WalletChecker.tsx
+++ src/components/WalletChecker.tsx
@@ -53,7 +53,6 @@ const WalletChecker = () => {
     <div className="relative min-h-screen">
       {/* Snowfall effect */}
       <div className="fixed inset-0 pointer-events-none">
-      <p>TEST TEST TEST 4444</p>
         {[...Array(20)].map((_, i) => (
           <div
             key={i}
@@ -68,7 +67,7 @@ const WalletChecker = () => {
       </div>
 
       <div className="container max-w-2xl py-8">
-      <h1 className="text-6xl font-['Berkshire_Swash'] text-christmas-green text-center mb-4">
+      <h1 className="text-6xl font-['Berkshire_Swash'] text-christmas-red text-center mb-4">
           Santa's Blockchain List
         </h1>
         <p className="text-lg text-gray-700 text-center mb-8 font-['Inter']">
